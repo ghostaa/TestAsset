f80552d1cd6257bc9bdc270a682af1ea64ad1ba4
lyf <lyf@f384b902-b47a-0449-8858-c8eae1429e8d>
2013-03-12 09:16:57 +0000

    git-svn-id: file:///c/Repositories/TestAsset@55 f384b902-b47a-0449-8858-c8eae1429e8d

--- TestRESTChannel/TestRestChannel/src/com/ibm/btt/rest/test/TestOperationResourceData.java
+++ TestRESTChannel/TestRestChannel/src/com/ibm/btt/rest/test/TestOperationResourceData.java
@@ -1,24 +1,46 @@
 package com.ibm.btt.rest.test;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import javax.xml.datatype.XMLGregorianCalendar;
 
+import org.apache.wink.client.ClientResponse;
+import org.apache.wink.client.Resource;
 import org.apache.wink.json4j.JSONException;
 import org.apache.wink.json4j.JSONObject;
 import org.junit.Test;
 
 import com.ibm.btt.base.types.impl.Currency;
 
-public class TestOperationResourceData extends TestOperationResourceAPI {
+public class TestOperationResourceData extends TestResourceBase {
+	
+	private String operationUrl = "/operations";
+	// http://localhost:8080/TestRestChannel/rest/operations
+	private String operationName = "/restChannelsOp";
+	private String commonUrl = baseUrl + operationUrl + operationName;
+	private String dirUrl;
+	
+	JSONObject jsonObjectOriginal = TestUtil.getJSONObjectForOperation();
 
+	public Resource getResource(String dirUrl,String contentType) {
+		Resource resource = client.resource(dirUrl);
+		resource.header("Cookie", cookieString);
+		resource.accept("application/json");
+		resource.contentType(contentType + ";charset=UTF-8");
+		return resource;
+	}
+
+	
 	@Test
 	public void testOperationExecuteString() throws JSONException {
 		JSONObject json = new JSONObject();
 		json.put("stringDataOp", "ssssss");
 		jsonObjectOriginal.put("stringDataOp", "ssssss");
-		operationExecute(json, jsonObjectOriginal);
+//		operationExecute(json, jsonObjectOriginal);
 	}
 	
 	@Test
@@ -102,21 +124,44 @@ public class TestOperationResourceData extends TestOperationResourceAPI {
 	public void testOperationExecuteCurrency() throws JSONException {
 		String key = "currencyDataOp";
 		Currency value = new Currency("CN", new BigDecimal("99"));
-		operationExecuteType(key, value);
+//		operationExecuteType(key, value);
 	}
 
 	@Test
 	public void testOperationExecuteXMLGregorianCalendar() throws JSONException {
 		String key = "XMLGregorianCalendarDataOp";
 		XMLGregorianCalendar value = DataValue.convertToXMLGregorianCalendar();
-		operationExecuteType(key, value);
+//		operationExecuteType(key, value);
 	}
 	
 	
 	public void operationExecuteType(String key, Object value) throws JSONException {
 		JSONObject json = new JSONObject();
-		json.put(key, value);
-		jsonObjectOriginal.put(key, value);
-		operationExecute(json, jsonObjectOriginal);
+		json.put(key, value.toString());
+		jsonObjectOriginal.put(key, value.toString());
+		operationExecute(json, key, value);
+//		operationExecute(json, jsonObjectOriginal);
+	}
+	
+	public void operationExecute(JSONObject json, String key, Object jsonObjectOriginal) throws JSONException {
+		dirUrl = commonUrl;
+		String contentType = "application/json";
+		Resource resource = getResource(dirUrl, contentType);
+//		System.out.println(json);
+		ClientResponse response = resource.post(json);
+		if (response.getStatusCode() == 200) {
+			JSONObject res = response.getEntity(JSONObject.class);
+			TestUtil.removeDSEData(res.keys());
+//			System.out.println(res.toString());
+//			System.out.println(jsonObjectOriginal.toString());
+			assertTrue(res.getString(key).equals(jsonObjectOriginal.toString()));
+//			assertTrue(res.equals(jsonObjectOriginal));
+			log(res.toString());
+		} else {
+			String res = response.getEntity(String.class);
+			log(res.toString());
+			log(response.getStatusType());
+			fail("Response Status Code : " + response.getStatusCode());
+		}
 	}
 }
